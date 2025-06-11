290d863fee2e3621f053790a790923ab5a1e7b27
Roberto <roberto.cirivello@acrisureservices.com>
2025-06-11 16:44:53 +0200

    Aggiornamento dei file del sito

--- italia.php
+++ italia.php
@@ -1244,7 +1244,7 @@
             text-align: center;
             padding: 1.5rem 5%;
             border-top: 1px solid rgba(0, 0, 0, 0.1);
-            border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+            /* border-bottom: 1px solid rgba(0, 0, 0, 0.1); */
             margin: 0 auto;
             max-width: 1400px;
         }

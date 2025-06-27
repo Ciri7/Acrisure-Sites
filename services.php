a33a800c89d1b1b2043d38fa774d9c3c268370d7
Roberto <roberto.cirivello@acrisureservices.com>
2025-06-27 10:20:01 +0200

    Aggiornamento dei file del sito

--- services.php
+++ services.php
@@ -2413,6 +2413,7 @@
         </div>
     </section>
 
+    <!-- FOOTER -->
     <footer>
         <div class="footer-content">
             <div class="footer-column">
@@ -2421,7 +2422,7 @@
                     <li><i class="fas fa-file-contract"></i> Acrisure S.r.l. - REA MI-2616058</li>
                     <li><i class="fas fa-id-card"></i> P.IVA: 13147890969</li>
                     <li><i class="fas fa-map-marker-alt"></i> Via Giuseppe Mazzini, 3 - 07100 Sassari</li><br>
-                    <li><img src="/img/Acrisure Services.jpg" class="logo-footer" alt=""></li>
+                    <li><img src="/img/Acrisure Services.jpg.jpg" class="logo-footer" alt=""></li>
                 </ul>
             </div>
 

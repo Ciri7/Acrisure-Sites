742d2566378f80a7e7afa64ba8eb0d9dda119aa7
Roberto <roberto.cirivello@acrisureservices.com>
2025-07-10 12:28:07 +0200

    Aggiornamento dei file del sito

--- italia.php
+++ italia.php
@@ -1628,6 +1628,35 @@
             padding-left: 0.5rem;
             border-bottom-color: var(--accent);
         }
+
+        /* RECLAMI LINK---------- */
+
+        .footer-title-link {
+        color: var(--secondary);
+        text-decoration: none;
+        transition: all 0.3s;
+        display: inline-block;
+        position: relative;
+        }
+
+        .footer-title-link:hover {
+            color: var(--accent);
+        }
+
+        .footer-title-link::after {
+            content: '';
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 0;
+            height: 2px;
+            background-color: var(--accent);
+            transition: width 0.3s;
+        }
+
+        .footer-title-link:hover::after {
+            width: 50px;
+        }
     </style>
 </head>
 <body>
@@ -2496,7 +2525,7 @@
 
                 <br>
                 <div class="footer-column">
-                    <i class="fas fa-exclamation-circle"></i> <h3 href="/reclami/reclamiItalia.php">Reclami</h3>
+                    <h3><i class="fas fa-exclamation-circle"></i> <a href="reclami.php" class="footer-title-link">Reclami</a></h3>
                     <ul class="footer-links">
                         <li><i class="fas fa-envelope"></i> <a href="mailto:reclami@acrisureitalia.com">reclami@acrisureitalia.com</a></li>
                         <li><i class="fas fa-envelope"></i> <a href="mailto:acrisurespa@pec.acrisureitalia.com">acrisurespa@pec.acrisureitalia.com</a></li>

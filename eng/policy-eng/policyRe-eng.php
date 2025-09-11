7faa4c7f556d50f6cf8d72c06ba8dff663b7fc9b
Roberto <roberto.cirivello@acrisureservices.com>
2025-09-11 15:18:09 +0200

    Aggiornamento dei file del sito

--- eng/policy-eng/policyRe-eng.php
+++ eng/policy-eng/policyRe-eng.php
@@ -978,7 +978,7 @@
         <div class="footer-legal-notice">
             <p>Acrisure Re Italia S.r.l. - Sole shareholder company under the direction and coordination of Acrisure S.r.l. - Share Capital €120,000.00 fully paid.
                 Registered in Section B of the RUI under no. B000729790, it is subject to IVASS supervision.
-                The identification and registration data provided above can be verified by consulting the Single Register of Intermediaries (RUI) on the IVASS website: <a href="https://www.ivass.it">www.ivass.it</a>.
+                The identification and registration data provided above can be verified by consulting the Single Register of Intermediaries (RUI) on the IVASS website: <a href="https://ruipubblico.ivass.it/rui-pubblica/ng/#/home">www.ivass.it</a>.
                 Company participating in the Acrisure Group VAT number P.IVA 13147890969 - Tax Code and registration number in the Sassari Companies Register n. 02942110905 REA SS-217191
             </p>
         </div>
--- news/newsRe.php
+++ news/newsRe.php
@@ -1134,7 +1134,7 @@
         <div class="footer-legal-notice">
             <p>Acrisure Re Italia S.r.l. - Società con socio unico soggetta alla direzione e coordinamento di Acrisure S.r.l. - Cap.sociale € 120.000,00 i.v.
                 Iscritta alla Sez. B del RUI n° B000729790, è soggetta alla vigilanza IVASS.
-                I dati identificativi e di iscrizione sopra forniti possono essere verificati consultando il Registro Unico degli Intermediari (RUI) sul sito Ivass: <a href="www.ivass.it">www.ivass.it</a>.
+                I dati identificativi e di iscrizione sopra forniti possono essere verificati consultando il Registro Unico degli Intermediari (RUI) sul sito Ivass: <a href="https://ruipubblico.ivass.it/rui-pubblica/ng/#/home">www.ivass.it</a>.
                 Società partecipante al Gruppo IVA Acrisure P.IVA 13147890969- Cod. Fiscale e n. iscr.al registro Imprese di Sassari n.02942110905 REA SS-217191
             </p>
         </div>

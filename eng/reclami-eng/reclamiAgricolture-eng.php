98b0d05306775aff0f88a939140a77477a4c3e19
Roberto <roberto.cirivello@acrisureservices.com>
2025-09-29 12:06:32 +0200

    Aggiornamento dei file del sito

--- eng/reclami-eng/reclamiAgricolture-eng.php
+++ eng/reclami-eng/reclamiAgricolture-eng.php
@@ -853,11 +853,11 @@
             </div>
             <nav>
                 <ul>
-                    <li><a href="/agricolture-eng.php#news" class="nav-link">News</a></li>
-                    <li><a href="/agricolture-eng.php#gruppo" class="nav-link">Leadership</a></li>
-                    <li><a href="/agricolture-eng.php#services" class="nav-link">Services</a></li>
-                    <li><a href="/index-eng.php#gruppo" class="nav-link">Our Group</a></li>
-                    <li><a href="/lavora-con-noi/lcn-acrisure-eng.php" class="nav-link">Work with us</a></li>
+                    <li><a href="/eng/agricolture-eng.php#news" class="nav-link">News</a></li>
+                    <li><a href="/eng/agricolture-eng.php#gruppo" class="nav-link">Leadership</a></li>
+                    <li><a href="/eng/agricolture-eng.php#services" class="nav-link">Services</a></li>
+                    <li><a href="/eng/index-eng.php#gruppo" class="nav-link">Our Group</a></li>
+                    <li><a href="/eng/lavora-con-noi/lcn-acrisure-eng.php" class="nav-link">Work with us</a></li>
                 </ul>
             </nav>
             <nav class="language-switcher">
